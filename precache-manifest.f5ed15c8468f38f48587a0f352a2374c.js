59c7c816d6c27262d7a33cede95294d5e2c60aa2
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-09-13 16:52:18 +0100

    Updates

---
+++ precache-manifest.f5ed15c8468f38f48587a0f352a2374c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c35255554a2d56914849fb8b81a8a14",
+    "url": "/dnv_frame_designer/index.html"
+  },
+  {
+    "revision": "e09eee550cbc5ddc69fd",
+    "url": "/dnv_frame_designer/static/css/main.6f1bc040.chunk.css"
+  },
+  {
+    "revision": "a912f3dace228c1ae038",
+    "url": "/dnv_frame_designer/static/js/2.9c620283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnv_frame_designer/static/js/2.9c620283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09eee550cbc5ddc69fd",
+    "url": "/dnv_frame_designer/static/js/main.82a09f79.chunk.js"
+  },
+  {
+    "revision": "9fb9e0d66fabff97352d",
+    "url": "/dnv_frame_designer/static/js/runtime-main.c88f4ceb.js"
+  }
+]);
