b5823906cf58eff86e9521c51e215601040cdcae
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-09-14 16:19:12 +0100

    Updates

---
+++ precache-manifest.2e634d831978c9a468c3a1fd870250ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0dc62deeee908ecbead12066dfa885",
+    "url": "/dnv_frame_designer/index.html"
+  },
+  {
+    "revision": "3548fff3caec9aee1f42",
+    "url": "/dnv_frame_designer/static/css/main.b951a49b.chunk.css"
+  },
+  {
+    "revision": "d31b20250aed961c56d1",
+    "url": "/dnv_frame_designer/static/js/2.41db7f5c.chunk.js"
+  },
+  {
+    "revision": "efa2bfe00d36dd44fc09",
+    "url": "/dnv_frame_designer/static/js/3.745a5c8b.chunk.js"
+  },
+  {
+    "revision": "b51ed37b171a34c94200ff01ec170480",
+    "url": "/dnv_frame_designer/static/js/3.745a5c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f9a8e1ddcbf785cd5b",
+    "url": "/dnv_frame_designer/static/js/4.364be9ad.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/dnv_frame_designer/static/js/4.364be9ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3548fff3caec9aee1f42",
+    "url": "/dnv_frame_designer/static/js/main.b98c0748.chunk.js"
+  },
+  {
+    "revision": "05d9efaa374c5105dbf9",
+    "url": "/dnv_frame_designer/static/js/runtime-main.82a40e51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dnv_frame_designer/precache-manifest.1840e86cb5aad31cc99607e99f3a5028.js"
+  "/dnv_frame_designer/precache-manifest.2e634d831978c9a468c3a1fd870250ae.js"
 );
 
 self.addEventListener('message', (event) => {
