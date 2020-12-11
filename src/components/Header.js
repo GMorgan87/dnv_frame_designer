36f265e50abb46584dbd5083035468d83dd8a957
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-12-11 17:10:59 +0000

    fixes link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="site-header">
-      <h2 ><a href="/">DNV Frame Designer</a></h2>
+      <h2 ><a href="/dnv_frame_designer/">DNV Frame Designer</a></h2>
     </div>
   )
 }
